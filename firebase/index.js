a0d5d955af1b675c29c351f065c21bbbdb0acc47
Travis Falls <369837+N3rd007@users.noreply.github.com>
2021-08-16 15:19:16 -0400

    Update index.js

--- firebase/index.js
+++ firebase/index.js
@@ -3,7 +3,7 @@ const admin = require('firebase-admin');
 const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT_CREDS;
 console.log('serviceAccount (Encoded): ' + serviceAccount);
 
-const fireBaseConfig = JSON.parse(Buffer.from(serviceAccount, 'base64').toString('ascii')).toString();
+const fireBaseConfig = JSON.parse(Buffer.from(serviceAccount, 'base64').toString('ascii').replace(/\\n/g, '\n')).toString();
 
 console.log('serviceAccount: ' + fireBaseConfig);
 
