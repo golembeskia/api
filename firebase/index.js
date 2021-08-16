1264604863a56aa3c359f6fb193d238b6ac2ea2f
Travis Falls <369837+N3rd007@users.noreply.github.com>
2021-08-16 15:27:17 -0400

    Update index.js

--- firebase/index.js
+++ firebase/index.js
@@ -3,22 +3,19 @@ var firebaseAdmin = require('firebase-admin');
 var serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT_CREDS;
 console.log('serviceAccount (Encoded): ' + serviceAccount);
 
-var rawFireBaseConfig = Buffer.from(serviceAccount, 'base64').toString('ascii');
+var rawFireBaseConfig = Buffer.from(serviceAccount, 'base64').toString('ascii').replace(/\\n/g, '\n');
 console.log('serviceAccount (Buffer Value): ' + rawFireBaseConfig);
 
-rawFireBaseConfig = rawFireBaseConfig.replace(/\\n/g, '\n');
-console.log('serviceAccount (Cleaned): ' + rawFireBaseConfig);
+//var fireBaseConfig = JSON.parse(rawFireBaseConfig);
+//console.log('serviceAccount (JSON parse): ' + fireBaseConfig);
 
-var fireBaseConfig = JSON.parse(rawFireBaseConfig);
-console.log('serviceAccount (JSON parse): ' + fireBaseConfig);
-
-fireBaseConfig = fireBaseConfig.toString();
-console.log('serviceAccount (Encoded): ' + fireBaseConfig);
+//fireBaseConfig = fireBaseConfig.toString();
+//console.log('serviceAccount (Encoded): ' + fireBaseConfig);
 
 if (!serviceAccount) throw new Error('The FIREBASE_SERVICE_ACCOUNT_CREDS environment variable was not found!');
 
 firebaseAdmin.initializeApp({
-  credential: firebaseAdmin.credential.cert(fireBaseConfig)
+  credential: firebaseAdmin.credential.cert(rawFireBaseConfig)
 })
 
 module.exports = firebaseAdmin;
