57ff6b5abf293d567b895b8b3bd4e56f7fb97d28
golembeskia <68037849+golembeskia@users.noreply.github.com>
2021-08-16 19:16:28 -0400

    Update index.js
    removed JSON.stringify from the const Firebase_Config. Also updated the credential on line 23

--- firebase/index.js
+++ firebase/index.js
@@ -1,6 +1,7 @@
 var firebaseAdmin = require('firebase-admin');
 
-const Firebase_Config = JSON.stringify({
+//const Firebase_Config = JSON.stringify({
+const Firebase_Config = ({
   "type": process.env.FIREBASE_TYPE,
   "project_id": process.env.FIREBASE_PROJECT_ID,
   "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
@@ -18,7 +19,8 @@ const Firebase_Config = JSON.stringify({
 if (!Firebase_Config) throw new Error('The FIREBASE_SERVICE_ACCOUNT_CREDS environment variable was not found!');
 
 firebaseAdmin.initializeApp({
-  credential: firebaseAdmin.credential.cert(Firebase_Config.toString())
+  //credential: firebaseAdmin.credential.cert(Firebase_Config.toString())
+  credential: firebaseAdmin.credential.cert(JSON.parse(Firebase_Config))
 })
 
 module.exports = firebaseAdmin;
