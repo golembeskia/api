77a90f096fc4b9f3a9f31525aa2de0728a02c1a9
Travis Falls <369837+N3rd007@users.noreply.github.com>
2021-08-16 13:13:14 -0400

    Updating changes for environment variables
    Updated the code to look for an environment variable that is base 64 encoded called FIREBASE_SERVICE_ACCOUNT_CREDS

--- firebase/index.js
+++ firebase/index.js
@@ -1,9 +1,10 @@
-var admin = require("firebase-admin");
+const admin = require('firebase-admin');
 
-var serviceAccount = require("../config/fbServiceAccountKey.json");
+const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT_CREDS;
+if (!serviceAccount) throw new Error('The FIREBASE_SERVICE_ACCOUNT_CREDS environment variable was not found!');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+  credential: admin.credential.cert(JSON.parse(Buffer.from(serviceAccount, 'base64').toString('ascii'))),
+})
 
 module.exports = admin;
