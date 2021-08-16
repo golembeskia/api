26565fb06714e2223e8599adfb4fe943179df9f1
Travis Falls <369837+N3rd007@users.noreply.github.com>
2021-08-16 17:55:50 -0400

    Update index.js

--- firebase/index.js
+++ firebase/index.js
@@ -2,8 +2,8 @@ var firebaseAdmin = require('firebase-admin');
 
 const Firebase_Config = JSON.stringify({
   "project_id": process.env.FIREBASE_PROJECT_ID,
-  "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID.replace(/\\n/g, '\n'),
-  "private_key": "",
+  "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+  "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
   "client_email": process.env.FIREBASE_CLIENT_EMAIL
 });
 
