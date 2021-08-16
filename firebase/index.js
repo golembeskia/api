076c957e06d734e4b7e16979d533cd2a287be36f
Travis Falls <369837+N3rd007@users.noreply.github.com>
2021-08-16 13:39:32 -0400

    Update index.js

--- firebase/index.js
+++ firebase/index.js
@@ -1,6 +1,9 @@
 const admin = require('firebase-admin');
 
 const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT_CREDS;
+console.log('serviceAccount (Encoded): ' + serviceAccount);
+console.log('serviceAccount: ' + JSON.parse(Buffer.from(serviceAccount, 'base64').toString('ascii')));
+
 if (!serviceAccount) throw new Error('The FIREBASE_SERVICE_ACCOUNT_CREDS environment variable was not found!');
 
 admin.initializeApp({
