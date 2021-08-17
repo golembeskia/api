c6e4cad93cc63551c96332eff2f1b26e7f8bada0
golembeskia <68037849+golembeskia@users.noreply.github.com>
2021-08-17 10:12:39 -0400

    Update index.js
    Made changes based on the question asked on stack overflow here: https://stackoverflow.com/questions/68816709/firbase-heroku-node-js-firebase-invalid-credential-failed-to-parse-service-a/68816799#68816799

--- firebase/index.js
+++ firebase/index.js
@@ -21,19 +21,35 @@ const fireBase_Config = JSON.stringify(fireBase_Data);
 if (!fireBaseAdmin) 
   throw new Error('The FIREBASE_SERVICE_ACCOUNT_CREDS environment variable was not found!');
 
+// fireBaseAdmin.initializeApp({
+//   "credential": fireBaseAdmin.credential.cert(JSON.stringify({
+//     "type": process.env.FIREBASE_TYPE,
+//     "project_id": process.env.FIREBASE_PROJECT_ID,
+//     "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+//     "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+//     "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+//     "client_id": process.env.FIREBASE_CLIENT_ID,
+//     "auth_uri": process.env.FIREBASE_AUTH_URI,
+//     "token_uri": process.env.FIREBASE_TOKEN_URI,
+//     "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER,
+//     "client_x509_cert_url": process.env.FIREBASE_CLIENT
+//   }))
+// });
+
 fireBaseAdmin.initializeApp({
-  "credential": fireBaseAdmin.credential.cert(JSON.stringify({
-    "type": process.env.FIREBASE_TYPE,
-    "project_id": process.env.FIREBASE_PROJECT_ID,
-    "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
-    "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
-    "client_id": process.env.FIREBASE_CLIENT_ID,
-    "auth_uri": process.env.FIREBASE_AUTH_URI,
-    "token_uri": process.env.FIREBASE_TOKEN_URI,
-    "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER,
-    "client_x509_cert_url": process.env.FIREBASE_CLIENT
-  }))
+  credential: admin.credential.cert({
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT
+  }),
+  databaseURL: `https://${process.env.FIREBASE_PROJECT_ID}.firebaseio.com/`
 });
 
 module.exports = fireBaseAdmin;
