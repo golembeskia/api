20f6248936a051004d5ac0bc2d1ba9475117ff12
Travis Falls <369837+N3rd007@users.noreply.github.com>
2021-08-17 00:57:04 -0400

    Update index.js

--- firebase/index.js
+++ firebase/index.js
@@ -4,7 +4,7 @@ const fireBase_Data = {
   "type": process.env.FIREBASE_TYPE,
   "project_id": process.env.FIREBASE_PROJECT_ID,
   "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
-  "private_key": process.env.FIREBASE_PRIVATE_KEY,
+  "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
   "client_email": process.env.FIREBASE_CLIENT_EMAIL,
   "client_id": process.env.FIREBASE_CLIENT_ID,
   "auth_uri": process.env.FIREBASE_AUTH_URI,
@@ -20,7 +20,7 @@ if (!fireBase_Config)
   throw new Error('The FIREBASE_SERVICE_ACCOUNT_CREDS environment variable was not found!');
 
 fireBaseAdmin.initializeApp({
-  credential: fireBaseAdmin.credential.cert(fireBase_Config.toString())
+  credential: fireBaseAdmin.credential.cert(fireBase_Config)
 });
 
 module.exports = fireBaseAdmin;
