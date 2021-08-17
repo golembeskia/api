cbb08b0d85b3cad3d08e8f2ca0674008acf5bbec
Travis Falls <369837+N3rd007@users.noreply.github.com>
2021-08-17 00:47:25 -0400

    Update index.js

--- firebase/index.js
+++ firebase/index.js
@@ -1,27 +1,26 @@
-var firebaseAdmin = require('firebase-admin');
+var fireBaseAdmin = require('firebase-admin');
 
-const Firebase_Config = JSON.stringify({
-//const Firebase_Config = ({
+var fireBase_Data = {
   "type": process.env.FIREBASE_TYPE,
   "project_id": process.env.FIREBASE_PROJECT_ID,
   "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
-  "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  "private_key": process.env.FIREBASE_PRIVATE_KEY,
   "client_email": process.env.FIREBASE_CLIENT_EMAIL,
   "client_id": process.env.FIREBASE_CLIENT_ID,
   "auth_uri": process.env.FIREBASE_AUTH_URI,
   "token_uri": process.env.FIREBASE_TOKEN_URI,
   "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER,
   "client_x509_cert_url": process.env.FIREBASE_CLIENT
-});
+}
+
+var fireBase_Config = JSON.stringify(fireBase_Data);
 
 //.replace(/\\n/g, '\n')
+if (!fireBase_Config) 
+  throw new Error('The FIREBASE_SERVICE_ACCOUNT_CREDS environment variable was not found!');
 
-if (!Firebase_Config) throw new Error('The FIREBASE_SERVICE_ACCOUNT_CREDS environment variable was not found!');
+fireBaseAdmin.initializeApp({
+  credential: fireBaseAdmin.credential.cert(fireBase_Config.toString())
+});
 
-firebaseAdmin.initializeApp({
-  credential: firebaseAdmin.credential.cert(Firebase_Config.toString())
-  //credential: firebaseAdmin.credential.cert(JSON.stringify(Firebase_Config))
-  //credential: firebaseAdmin.credential.cert(JSON.parse(Firebase_Config))
-})
-
-module.exports = firebaseAdmin;
+module.exports = fireBaseAdmin;
